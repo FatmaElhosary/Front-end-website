baa978c118ed0bb74b4554b980e2effd1eba9b2e
FatmaElhosary <engfatmafathy@gmail.com>
2020-04-28 14:19:56 +0200

    update nav

--- js/MYJS.js
+++ js/MYJS.js
@@ -1,7 +1,20 @@
-function onscroll(){
-    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350)
-        document.querySelector("nav").style.backgroundColor="red";
-       
-    
-}
-   
+/* window.onscroll = function() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >50)
+    {
+        document.querySelector("nav").style.backgroundColor="white";
+        this.document.querySelector(".navbar-light .navbar-nav .nav-link ").style.color="black  !important";
+
+    }else{
+
+    }
+      
+
+}; */
+
+$(window).on('scroll',function(){
+    if($(window).scrollTop()){
+        $('nav').addClass('black');
+    }else{
+        $('nav').removeClass('black');
+    }
+})
