3df7aed989a8ef7176692ade07d7257102e50a36
FatmaElhosary <engfatmafathy@gmail.com>
2020-05-05 20:06:45 +0200

    making theme color of this website

--- js/MYJS.js
+++ js/MYJS.js
@@ -11,10 +11,40 @@
 
 }; */
 
-$(window).on('scroll',function(){
-    if($(window).scrollTop()){
-        $('nav').addClass('black');
-    }else{
-        $('nav').removeClass('black');
-    }
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $("nav").addClass("black");
+  } else {
+    $("nav").removeClass("black");
+  }
+});
+//JQuery
+$(".home-setting").click(function () {
+var boxColorWidth=$(".color-palet").outerWidth();//width+border+padding of color-palet
+var boxLeft=$('.color-setting').css("left"); //settinig left of .color-setting 
+//$('.color-palet').toggle();
+ if(boxLeft=="0px"){
+    $('.color-setting').animate({left:`-${boxColorWidth}`},1000);
+}else{
+    $('.color-setting').animate({left:"0px"},1000);
+  
+} 
+});
+
+$('.color-palet ul li').click(function(){
+  var themecolor=$(this).css("backgroundColor");
+  $(':root').css('--theme-color',themecolor);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
